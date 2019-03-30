a5642016c839e7d92aec05315473eb05e0852399
unknown <deepu1guru@gmail.com>
2019-03-30 22:38:42 +0530

    Under Construction Index init

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Deepak Guru </title>
+  </head>
+  <body>
+    <p>Hello!<br>Welcome to my website.
+      <br><center><b>Under Development</b></center><br>
+      Stay tuned...
+    </p>
+  </body>
+</html>
